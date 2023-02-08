c75187fe06bf745cdb21631ed5ea43a97d9a6caf
Foretack <observer.in.a.box@gmail.com>
2023-02-08 11:24:51 +0100

    Update delete messages extension

--- Utils/MemberExtensions.cs
+++ Utils/MemberExtensions.cs
@@ -1,30 +1,15 @@
-﻿using System.Buffers;
-using DSharpPlus.Entities;
+﻿using DSharpPlus.Entities;
 
 namespace SFFBot.Utils;
 internal static class MemberExtensions
 {
     public static async Task DeleteMessages(this DiscordMember member)
     {
-        const int maxAmount = 128;
-        int index = 0;
-        DiscordChannel[] channels = member.Guild.Channels.Select(x => x.Value).ToArray();
-        ulong userid = member.Id;
-
-        foreach (var channel in channels)
+        foreach (var e in Bot.MessageCache.Where(x => x.Author.Id == member.Id))
         {
-            var userMessages = ArrayPool<DiscordMessage>.Shared.Rent(maxAmount);
-
-            var messageList = await channel.GetMessagesAsync(maxAmount);
-            foreach (var message in messageList.Where(x => x.Author.Id == userid))
-            {
-                if (index >= maxAmount - 1) break;
-                userMessages[index++] = message;
-            }
-            await channel.DeleteMessagesAsync(userMessages);
-            Log.Information("Deleting messages from user {user} in channel {channel}", member.Username, channel.Name);
-
-            ArrayPool<DiscordMessage>.Shared.Return(userMessages, true);
+            if ((DateTime.Now - e.Message.CreationTimestamp).TotalDays >= 10) continue;
+            await e.Message.DeleteAsync();
+            await Task.Delay(250);
         }
     }
 }
